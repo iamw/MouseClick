2d03edd9514fb0a472ee4a3681bd53c46be7fa41
iamw <w521714725@163.com>
2015-12-17 23:46:01 +0800

    初步实现

--- MouseClick/Form1.Designer.cs
+++ MouseClick/Form1.Designer.cs
@@ -71,11 +71,12 @@
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("新宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.button1.Location = new System.Drawing.Point(142, 199);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(167, 52);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "开始点击";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
